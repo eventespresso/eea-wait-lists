e69f2be02ae56abff18b14aa60ae4130c237d205
Brent Christensen <brent@eventespresso.com>
2016-10-24 15:43:49 -0700

    add initial shell for EED_Wait_Lists module; [touch:10164]

---
+++ EED_Wait_Lists.module.php
@@ -0,0 +1,33 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+
+
+/**
+ * Class EED_Wait_Lists
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         1.0.0
+ */
+class EED_Wait_Lists extends EED_Module {
+
+
+
+	/**
+	 *    run - initial module setup
+	 *    this method is primarily used for activating resources in the EE_Front_Controller thru the use of filters
+	 *
+	 * @access    public
+	 * @var            WP $WP
+	 * @return    void
+	 */
+	public function run( $WP ) {
+		// TODO: Implement run() method.
+	}
+
+
+
+}
+// End of file EED_Wait_Lists.module.php
+// Location: wp-content/plugins/eea-wait-lists/EED_Wait_Lists.module.php

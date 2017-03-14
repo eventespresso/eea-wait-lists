455ba1afb7bd369c8a61727dbb52974858cb177b
Brent Christensen <brent@eventespresso.com>
2017-03-14 13:09:43 -0700

    PSR formatting; [touch:10171]

--- WaitListEventsCollection.php
+++ WaitListEventsCollection.php
@@ -1,10 +1,9 @@
 <?php
 namespace EventEspresso\WaitList;
 
-
 use EventEspresso\core\services\collections\Collection;
 
-defined( 'EVENT_ESPRESSO_VERSION' ) || exit;
+defined('EVENT_ESPRESSO_VERSION') || exit;
 
 
 
@@ -16,32 +15,34 @@ defined( 'EVENT_ESPRESSO_VERSION' ) || exit;
  * @author        Brent Christensen
  * @since         $VID:$
  */
-class WaitListEventsCollection extends Collection  {
+class WaitListEventsCollection extends Collection
+{
 
-	/**
-	 * ProgressStepCollection constructor.
-	 *
-	 * @throws \EventEspresso\core\exceptions\InvalidInterfaceException
-	 * @throws \EE_Error
-	 * @throws \EventEspresso\core\exceptions\InvalidEntityException
-	 */
-	public function __construct() {
-		parent::__construct( '\EE_Event' );
-		$wait_list_events = \EEM_Event::instance()->get_all(
-			array(
-				array(
-					'EVT_allow_overflow' => true
-				)
-			)
-		);
-		if ( ! empty($wait_list_events) && is_array($wait_list_events)) {
-			foreach ( $wait_list_events as $wait_list_event ) {
-				if ( $wait_list_event instanceof \EE_Event ) {
-					$this->add( $wait_list_event, $wait_list_event->ID() );
-				}
-			}
-		}
-	}
+    /**
+     * ProgressStepCollection constructor.
+     *
+     * @throws \EventEspresso\core\exceptions\InvalidInterfaceException
+     * @throws \EE_Error
+     * @throws \EventEspresso\core\exceptions\InvalidEntityException
+     */
+    public function __construct()
+    {
+        parent::__construct('\EE_Event');
+        $wait_list_events = \EEM_Event::instance()->get_all(
+            array(
+                array(
+                    'EVT_allow_overflow' => true,
+                ),
+            )
+        );
+        if (! empty($wait_list_events) && is_array($wait_list_events)) {
+            foreach ($wait_list_events as $wait_list_event) {
+                if ($wait_list_event instanceof \EE_Event) {
+                    $this->add($wait_list_event, $wait_list_event->ID());
+                }
+            }
+        }
+    }
 
 }
 // End of file WaitListEventsCollection.php

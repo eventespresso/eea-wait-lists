efe43a5dfb1212da7f7fc052cd208ad1c5406e85
Brent Christensen <brent@eventespresso.com>
2017-03-14 13:10:53 -0700

    PSR formatting; [touch:10164]

--- eea-wait-lists.php
+++ eea-wait-lists.php
@@ -37,77 +37,72 @@
  * ------------------------------------------------------------------------
  */
 // define versions and this file
-define( 'EE_WAIT_LISTS_CORE_VERSION_REQUIRED', '4.9.18.rc.001' );
-define( 'EE_WAIT_LISTS_VERSION', '1.0.0.dev.000' );
-define( 'EE_WAIT_LISTS_PLUGIN_FILE', __FILE__ );
-
-
-
+define('EE_WAIT_LISTS_CORE_VERSION_REQUIRED', '4.9.18.rc.001');
+define('EE_WAIT_LISTS_VERSION', '1.0.0.dev.000');
+define('EE_WAIT_LISTS_PLUGIN_FILE', __FILE__);
 /**
  *    captures plugin activation errors for debugging
  */
-function espresso_wait_lists_plugin_activation_errors() {
-	if ( WP_DEBUG && ob_get_length() > 0 ) {
-		$activation_errors = ob_get_contents();
-		file_put_contents(
-			EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS . 'espresso_wait_lists_plugin_activation_errors.html',
-			$activation_errors
-		);
-	}
+function espresso_wait_lists_plugin_activation_errors()
+{
+    if (WP_DEBUG && ob_get_length() > 0) {
+        $activation_errors = ob_get_contents();
+        file_put_contents(
+            EVENT_ESPRESSO_UPLOAD_DIR . 'logs' . DS . 'espresso_wait_lists_plugin_activation_errors.html',
+            $activation_errors
+        );
+    }
 }
-add_action( 'activated_plugin', 'espresso_wait_lists_plugin_activation_errors' );
 
-
-
+add_action('activated_plugin', 'espresso_wait_lists_plugin_activation_errors');
 /**
  *    registers addon with EE core
  */
-function load_espresso_wait_lists() {
-	if ( class_exists( 'EE_Addon' ) ) {
-		// wait_lists version
-		require_once( __DIR__ . DS . 'EE_Wait_Lists.class.php' );
-		EE_Wait_Lists::register_addon();
-	} else {
-		add_action( 'admin_notices', 'espresso_wait_lists_activation_error' );
-	}
+function load_espresso_wait_lists()
+{
+    if (class_exists('EE_Addon')) {
+        // wait_lists version
+        require_once(__DIR__ . DS . 'EE_Wait_Lists.class.php');
+        EE_Wait_Lists::register_addon();
+    } else {
+        add_action('admin_notices', 'espresso_wait_lists_activation_error');
+    }
 }
-add_action( 'AHEE__EE_System__load_espresso_addons', 'load_espresso_wait_lists' );
 
-
-
+add_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_wait_lists');
 /**
  *    verifies that addon was activated
  */
-function espresso_wait_lists_activation_check() {
-	if ( ! did_action( 'AHEE__EE_System__load_espresso_addons' ) ) {
-		add_action( 'admin_notices', 'espresso_wait_lists_activation_error' );
-	}
+function espresso_wait_lists_activation_check()
+{
+    if (! did_action('AHEE__EE_System__load_espresso_addons')) {
+        add_action('admin_notices', 'espresso_wait_lists_activation_error');
+    }
 }
-add_action( 'init', 'espresso_wait_lists_activation_check', 1 );
 
-
-
+add_action('init', 'espresso_wait_lists_activation_check', 1);
 /**
  *    displays activation error admin notice
  */
-function espresso_wait_lists_activation_error() {
-	unset( $_GET['activate'], $_REQUEST['activate'] );
-	if ( ! function_exists( 'deactivate_plugins' ) ) {
-		require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
-	}
-	deactivate_plugins( plugin_basename( EE_WAIT_LISTS_PLUGIN_FILE ) );
-	?>
-	<div class="error">
-		<p><?php printf(
-				esc_html__(
-					'Event Espresso Wait Lists could not be activated. Please ensure that Event Espresso version %1$s or higher is running',
-					'event_espresso'
-				),
-				EE_WAIT_LISTS_CORE_VERSION_REQUIRED
-			);
-			?></p>
-	</div>
-	<?php
+function espresso_wait_lists_activation_error()
+{
+    unset($_GET['activate'], $_REQUEST['activate']);
+    if (! function_exists('deactivate_plugins')) {
+        require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+    }
+    deactivate_plugins(plugin_basename(EE_WAIT_LISTS_PLUGIN_FILE));
+    ?>
+    <div class="error">
+        <p><?php printf(
+                esc_html__(
+                    'Event Espresso Wait Lists could not be activated. Please ensure that Event Espresso version %1$s or higher is running',
+                    'event_espresso'
+                ),
+                EE_WAIT_LISTS_CORE_VERSION_REQUIRED
+            );
+            ?></p>
+    </div>
+    <?php
 }
 
 

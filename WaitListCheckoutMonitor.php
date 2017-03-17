f44adb1b40e2ebf09af82eb7388b121dc8e17b7e
Brent Christensen <brent@eventespresso.com>
2017-03-17 16:33:27 -0700

    action callback returns nothing; somehow managed to delete filter for the initializeTxnRegStepsArray() callback, AND wasn't actually saving reg steps to the TXN, so fixed both of those things; [touch:10168]

--- WaitListCheckoutMonitor.php
+++ WaitListCheckoutMonitor.php
@@ -25,19 +25,18 @@ class WaitListCheckoutMonitor
      * checks if
      *
      * @param EE_Checkout $checkout
-     * @return EE_Checkout
      * @throws EE_Error
      */
     public function loadAndInstantiateRegSteps(EE_Checkout $checkout)
     {
         // revisits should function the same as they always do
         if ($checkout->revisit) {
-            return $checkout;
+            return;
         }
         // not a revisit but there's a reg_url_link ?
         $registration = EEM_Registration::instance()->get_registration_for_reg_url_link($checkout->reg_url_link);
         if (! $registration instanceof \EE_Registration) {
-            return $checkout;
+            return;
         }
         // ok, but were they ever on a wait list ?
         if ($registration->get_extra_meta(WaitList::REG_SIGNED_UP_META_KEY, true) !== null) {
@@ -45,8 +44,11 @@ class WaitListCheckoutMonitor
             add_filter('FHEE__Single_Page_Checkout__load_reg_steps__reload_reg_steps', '__return_true');
             // and do NOT bypass loading for any of them, the registrant needs to visit each step
             add_filter('FHEE__Single_Page_Checkout___load_and_instantiate_reg_step__bypass_reg_step', '__return_false');
+            add_filter(
+                'FHEE__EED_Single_Page_Checkout___final_verifications__checkout',
+                array($this, 'initializeTxnRegStepsArray')
+            );
         }
-        return $checkout;
     }
 
 
@@ -56,10 +58,13 @@ class WaitListCheckoutMonitor
      *
      * @param EE_Checkout $checkout
      * @return EE_Checkout
+     * @throws EE_Error
      */
     public function initializeTxnRegStepsArray(EE_Checkout $checkout)
     {
-        $checkout->initialize_txn_reg_steps_array();
+        $checkout->transaction->set_reg_steps(
+            $checkout->initialize_txn_reg_steps_array()
+        );
         return $checkout;
     }
 

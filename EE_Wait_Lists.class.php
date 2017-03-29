cfa24dd9cf576ad25a7b1ec04144d9d4db15932f
Brent Christensen <brent@eventespresso.com>
2017-03-29 08:17:49 -0700

    use $this when calling non-static methods; use type safe comparison; PSR formatting; [touch:10171]

--- EE_Wait_Lists.class.php
+++ EE_Wait_Lists.class.php
@@ -44,22 +44,22 @@ Class  EE_Wait_Lists extends EE_Addon
                 ),
                 'module_paths'     => array(
                     EE_WAIT_LISTS_PATH . 'EED_Wait_Lists.module.php',
-                    EE_WAIT_LISTS_PATH . 'EED_Wait_Lists_Messages.module.php'
+                    EE_WAIT_LISTS_PATH . 'EED_Wait_Lists_Messages.module.php',
                 ),
-                'message_types' => array(
+                'message_types'    => array(
                     'waitlist_can_register' => array(
-                        'mtfilename' => 'EE_Waitlist_Can_Register_message_type.class.php',
-                        'autoloadpaths' => array(
-                            EE_WAIT_LISTS_PATH . 'messages/'
+                        'mtfilename'                                       => 'EE_Waitlist_Can_Register_message_type.class.php',
+                        'autoloadpaths'                                    => array(
+                            EE_WAIT_LISTS_PATH . 'messages/',
                         ),
-                        'messengers_to_activate_with' => array('email'),
-                        'messengers_to_validate_with' => array('email'),
-                        'force_activation' => true,
+                        'messengers_to_activate_with'                      => array('email'),
+                        'messengers_to_validate_with'                      => array('email'),
+                        'force_activation'                                 => true,
                         'messengers_supporting_default_template_pack_with' => array('email'),
-                        'base_path_for_default_templates' => EE_WAIT_LISTS_PATH . 'messages/templates/',
-                        'base_path_for_default_variation' => EE_WAIT_LISTS_PATH . 'messages/templates/variations/',
-                        'base_url_for_default_variation' => EE_WAIT_LISTS_URL . 'messages/templates/variations/'
-                    )
+                        'base_path_for_default_templates'                  => EE_WAIT_LISTS_PATH . 'messages/templates/',
+                        'base_path_for_default_variation'                  => EE_WAIT_LISTS_PATH . 'messages/templates/variations/',
+                        'base_url_for_default_variation'                   => EE_WAIT_LISTS_URL . 'messages/templates/variations/',
+                    ),
                 ),
                 // if plugin update engine is being used for auto-updates. not needed if PUE is not being used.
                 'pue_options'      => array(
@@ -73,12 +73,13 @@ Class  EE_Wait_Lists extends EE_Addon
     }
 
 
+
     /**
      * Register things that have to happen early in loading.
      */
     public function after_registration()
     {
-        self::_register_custom_shortcode_library();
+        $this->_register_custom_shortcode_library();
         add_action(
             'FHEE__EE_Messages_Base__get_valid_shortcodes',
             array(__CLASS__, 'modify_valid_shortcodes'),
@@ -88,6 +89,7 @@ Class  EE_Wait_Lists extends EE_Addon
     }
 
 
+
     /**
      * Takes care of registering the custom shortcode library for this add-on
      */
@@ -95,30 +97,37 @@ Class  EE_Wait_Lists extends EE_Addon
     {
         //ya intentionally using closures here.  If client code want's this library to not be registered there's
         //facility for deregistering via the provided api.  This forces client code to use that api.
-        add_action('EE_Brewing_Regular___messages_caf', function () {
-            EE_Register_Messages_Shortcode_Library::register(
-                'recipient_waitlist_shortcode_library',
-                array(
-                    'name'                    => 'recipient_waitlist',
-                    'autoloadpaths'           => EE_WAIT_LISTS_PATH . 'messages/shortcodes/',
-                    'msgr_validator_callback' => array(__CLASS__, 'messenger_validator_callback'),
-                )
-            );
-        }, 20);
-
+        add_action(
+            'EE_Brewing_Regular___messages_caf',
+            function () {
+                EE_Register_Messages_Shortcode_Library::register(
+                    'recipient_waitlist_shortcode_library',
+                    array(
+                        'name'                    => 'recipient_waitlist',
+                        'autoloadpaths'           => EE_WAIT_LISTS_PATH . 'messages/shortcodes/',
+                        'msgr_validator_callback' => array(__CLASS__, 'messenger_validator_callback'),
+                    )
+                );
+            },
+            20
+        );
         //make sure the shortcode library is deregistered if this add-on is deregistered.
-        add_action('AHEE__EE_Register_Addon__deregister__after', function ($addon_name) {
-            if ($addon_name === 'Wait_Lists') {
-                EE_Register_Messages_Shortcode_Library::deregister('recipient_waitlist_shortcode_library');
+        add_action(
+            'AHEE__EE_Register_Addon__deregister__after',
+            function ($addon_name) {
+                if ($addon_name === 'Wait_Lists') {
+                    EE_Register_Messages_Shortcode_Library::deregister('recipient_waitlist_shortcode_library');
+                }
             }
-        });
+        );
     }
+
 
 
     /**
      * Callback on `FHEE__EE_Messages_Base__get_valid_shortcodes` that is used to ensure the new shortcode library is
      * registered with the appropriate message type as a valid library.
-     * Also using this to remove shotcodes we don't want exposed for the new message type.
+     * Also using this to remove shortcodes we don't want exposed for the new message type.
      *
      * @param array           $valid_shortcodes Existing array of valid shortcodes
      * @param EE_message_type $message_type
@@ -128,7 +137,7 @@ Class  EE_Wait_Lists extends EE_Addon
     {
         if ($message_type instanceof EE_WaitList_Can_Register_message_type) {
             $valid_shortcodes['attendee'][] = 'recipient_waitlist';
-            $shortcode_libraries_to_remove  = array(
+            $shortcode_libraries_to_remove = array(
                 'primary_registration_details',
                 'primary_registration_list',
                 'question_list',
@@ -141,7 +150,8 @@ Class  EE_Wait_Lists extends EE_Addon
                 function ($shortcode_library_to_remove) use (&$valid_shortcodes) {
                     $key_to_remove = array_search(
                         $shortcode_library_to_remove,
-                        $valid_shortcodes['attendee']
+                        $valid_shortcodes['attendee'],
+                        true
                     );
                     if ($key_to_remove !== false) {
                         unset($valid_shortcodes['attendee'][$key_to_remove]);
@@ -151,6 +161,7 @@ Class  EE_Wait_Lists extends EE_Addon
         }
         return $valid_shortcodes;
     }
+
 
 
     /**
@@ -165,7 +176,6 @@ Class  EE_Wait_Lists extends EE_Addon
         if ($messenger->name !== 'email') {
             return $validator_config;
         }
-
         array_push(
             $validator_config['content']['shortcodes'],
             'recipient_waitlist',

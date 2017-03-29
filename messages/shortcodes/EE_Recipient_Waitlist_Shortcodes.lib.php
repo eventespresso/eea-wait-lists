946e0e1494e86b482cb41a5e4bb86ba238402757
Brent Christensen <brent@eventespresso.com>
2017-03-29 08:15:09 -0700

    declare all exceptions; PSR formatting; [touch:10171]

--- messages/shortcodes/EE_Recipient_Waitlist_Shortcodes.lib.php
+++ messages/shortcodes/EE_Recipient_Waitlist_Shortcodes.lib.php
@@ -1,6 +1,8 @@
 <?php
 defined('EVENT_ESPRESSO_VERSION') || exit('No direct access allowed.');
 
+
+
 /**
  * Shortcode library for registration waitlist message types
  *
@@ -19,12 +21,13 @@ class EE_Recipient_Waitlist_Shortcodes extends EE_Shortcodes
     protected $_recipient;
 
 
+
     /**
      * Initialize main properties for library.
      */
     protected function _init_props()
     {
-        $this->label       = esc_html__('Recipient Waitlist Shortcodes', 'event_espresso');
+        $this->label = esc_html__('Recipient Waitlist Shortcodes', 'event_espresso');
         $this->description = esc_html__(
             'These are shortcodes that are specific to the Waitlist message type.',
             'event_espresso'
@@ -38,11 +41,13 @@ class EE_Recipient_Waitlist_Shortcodes extends EE_Shortcodes
     }
 
 
+
     /**
      * Parser for shortcode
      *
      * @param string $shortcode Shortcode being parsed
      * @return string            Results of parser.
+     * @throws EE_Error
      */
     protected function _parser($shortcode)
     {
@@ -52,18 +57,16 @@ class EE_Recipient_Waitlist_Shortcodes extends EE_Shortcodes
                             && is_array($this->_data)
                             && isset($this->_data['data'])
                             && $this->_data['data'] instanceof EE_Messages_Addressee
-            ? $this->_data['data'] :
-            $this->_recipient;
+            ? $this->_data['data']
+            : $this->_recipient;
         $this->_recipient = ! $this->_recipient instanceof EE_Messages_Addressee
                             && ! empty($this->_extra_data['data'])
                             && $this->_extra_data['data'] instanceof EE_Messages_Addressee
             ? $this->_extra_data['data']
             : $this->_recipient;
-
         if (! $this->_recipient instanceof EE_Messages_Addressee) {
             return '';
         }
-
         switch ($shortcode) {
             case '[RECIPIENT_WAITLIST_REGISTRATION_URL]':
                 if (! $this->_recipient->reg_obj instanceof EE_Registration) {

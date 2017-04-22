91b7f43b1fb085c549c8d114ee2f59c1174b65af
Darren Ethier <darren@roughsmootheng.in>
2017-04-22 16:32:40 -0400

    update test bootstrap/config for waitlists add-on [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,11 +6,15 @@
 use EETests\bootstrap\AddonLoader;
 
 $core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+//if still don't have $core_tests_dir, then let's check tmp folder.
+if (! is_dir($core_tests_dir)) {
+    $core_tests_dir = '/tmp/event-espresso-core/tests/';
+}
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 
 define('EE_WAITLISTS_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
-define('EE_WAITLISTS_TEST_DIR', EE_WAITLISTS_PLUGIN_DIR . 'tests');
+define('EE_WAITLISTS_TEST_DIR', EE_WAITLISTS_PLUGIN_DIR . 'tests/');
 
 
 $addon_loader = new AddonLoader(

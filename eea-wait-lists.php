f05e1d7e62cdd40a1c7d4a0a0ac3f3dbb23c362c
Brent Christensen <brent@eventespresso.com>
2017-05-02 12:18:42 -0700

    load and use new Constants class; [touch:10710]

--- eea-wait-lists.php
+++ eea-wait-lists.php
@@ -37,7 +37,6 @@
  * ------------------------------------------------------------------------
  */
 // define versions and this file
-define('EE_WAIT_LISTS_CORE_VERSION_REQUIRED', '4.9.18.rc.001');
 define('EE_WAIT_LISTS_VERSION', '1.0.0.rc.000');
 define('EE_WAIT_LISTS_PLUGIN_FILE', __FILE__);
 /**
@@ -55,14 +54,27 @@ function espresso_wait_lists_plugin_activation_errors()
 }
 
 add_action('activated_plugin', 'espresso_wait_lists_plugin_activation_errors');
+
+
+
 /**
  *    registers addon with EE core
  */
 function load_espresso_wait_lists()
 {
     if (class_exists('EE_Addon')) {
-        // wait_lists version
-        require_once(__DIR__ . DS . 'EE_Wait_Lists.class.php');
+        espresso_load_required(
+            'EventEspresso\WaitList\domain\Constants',
+            plugin_dir_path(EE_WAIT_LISTS_PLUGIN_FILE) . '/domain/Constants.php'
+        );
+        EventEspresso\WaitList\domain\Constants::init(
+            EE_WAIT_LISTS_PLUGIN_FILE,
+            EE_WAIT_LISTS_VERSION
+        );
+        espresso_load_required(
+            'EE_Wait_Lists',
+            EventEspresso\WaitList\domain\Constants::pluginPath() . 'EE_Wait_Lists.php'
+        );
         EE_Wait_Lists::register_addon();
     } else {
         add_action('admin_notices', 'espresso_wait_lists_activation_error');
@@ -70,6 +82,9 @@ function load_espresso_wait_lists()
 }
 
 add_action('AHEE__EE_System__load_espresso_addons', 'load_espresso_wait_lists');
+
+
+
 /**
  *    verifies that addon was activated
  */
@@ -81,6 +96,9 @@ function espresso_wait_lists_activation_check()
 }
 
 add_action('init', 'espresso_wait_lists_activation_check', 1);
+
+
+
 /**
  *    displays activation error admin notice
  */
@@ -98,7 +116,7 @@ function espresso_wait_lists_activation_error()
                     'Event Espresso Wait Lists could not be activated. Please ensure that Event Espresso version %1$s or higher is running',
                     'event_espresso'
                 ),
-                EE_WAIT_LISTS_CORE_VERSION_REQUIRED
+                EventEspresso\WaitList\domain\Constants::CORE_VERSION_REQUIRED
             );
             ?></p>
     </div>

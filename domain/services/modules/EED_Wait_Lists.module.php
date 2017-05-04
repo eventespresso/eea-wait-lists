e4c71686211ab001e29438ca10317b532a42aad7
Brent Christensen <brent@eventespresso.com>
2017-05-04 11:13:01 -0700

    update renamed class/namespace; [touch:10710]

--- domain/services/modules/EED_Wait_Lists.module.php
+++ domain/services/modules/EED_Wait_Lists.module.php
@@ -134,17 +134,17 @@ class EED_Wait_Lists extends EED_Module
         EE_Dependency_Map::register_dependencies(
             'EventEspresso\WaitList\domain\services\commands\PromoteWaitListRegistrantsCommandHandler',
             array(
-                'EEM_Registration' => EE_Dependency_Map::load_from_cache,
-                'EE_Capabilities' => EE_Dependency_Map::load_from_cache,
-                'EEM_Change_Log' => EE_Dependency_Map::load_from_cache,
-                'EventEspresso\core\services\commands\notices\CommandHandlerNotices' => EE_Dependency_Map::load_from_cache,
+                'EEM_Registration'                            => EE_Dependency_Map::load_from_cache,
+                'EE_Capabilities'                             => EE_Dependency_Map::load_from_cache,
+                'EEM_Change_Log'                              => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\services\notices\Notices' => EE_Dependency_Map::load_from_cache,
             )
         );
         EE_Dependency_Map::register_dependencies(
             'EventEspresso\WaitList\domain\services\commands\CreateWaitListRegistrationsCommandHandler',
             array(
-                'EventEspresso\core\services\commands\notices\CommandHandlerNotices' => EE_Dependency_Map::load_from_cache,
-                'EventEspresso\core\services\commands\CommandBusInterface' => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\services\notices\Notices'                  => EE_Dependency_Map::load_from_cache,
+                'EventEspresso\core\services\commands\CommandBusInterface'     => EE_Dependency_Map::load_from_cache,
                 'EventEspresso\core\services\commands\CommandFactoryInterface' => EE_Dependency_Map::load_from_cache,
             )
         );

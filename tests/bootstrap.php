efd5966ccbb5d6cfb96f73ce7caf80633fed0303
Brent Christensen <brent@eventespresso.com>
2017-05-04 14:01:59 -0700

    just use __DIR__; [touch:10710]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,9 @@
 /**
  * Bootstrap for eea-wait-lists tests
  */
-
 use EETests\bootstrap\AddonLoader;
 
-$core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+$core_tests_dir = dirname(dirname(__DIR__)) . '/event-espresso-core/tests/';
 //if still don't have $core_tests_dir, then let's check tmp folder.
 if (! is_dir($core_tests_dir)) {
     $core_tests_dir = '/tmp/event-espresso-core/tests/';
@@ -13,9 +12,8 @@ if (! is_dir($core_tests_dir)) {
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 
-define('EE_WAITLISTS_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
+define('EE_WAITLISTS_PLUGIN_DIR', dirname(__DIR__) . '/');
 define('EE_WAITLISTS_TEST_DIR', EE_WAITLISTS_PLUGIN_DIR . 'tests/');
-
 
 $addon_loader = new AddonLoader(
     EE_WAITLISTS_TEST_DIR,

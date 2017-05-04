51bda183b4151a7f595ea8c7fff3404c748983f8
Brent Christensen <brent@eventespresso.com>
2017-05-04 12:36:16 -0700

    bump core version required; [touch:10710]

--- domain/Constants.php
+++ domain/Constants.php
@@ -21,7 +21,7 @@ class Constants extends ConstantsAbstract
     /**
      * EE Core Version Required for Add-on
      */
-    const CORE_VERSION_REQUIRED = '4.9.18.rc.001';
+    const CORE_VERSION_REQUIRED = '4.9.39.p';
 
     /**
      * Event Meta Key used for tracking the number of spaces allocated for Wait List Registrations,

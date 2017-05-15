8b7bf21f45fbc639f54f6f5b745db7084ff8a422
Brent Christensen <brent@eventespresso.com>
2017-05-15 13:39:17 -0700

    add $wait_list_event_meta property, inject instance of WaitListEventMeta into constructor; [touch:10638]

--- domain/services/commands/WaitListCommandHandler.php
+++ domain/services/commands/WaitListCommandHandler.php
@@ -2,10 +2,8 @@
 
 namespace EventEspresso\WaitList\domain\services\commands;
 
-use EE_Error;
-use EE_Event;
 use EventEspresso\core\services\commands\CommandHandler;
-use EventEspresso\WaitList\domain\Constants;
+use EventEspresso\WaitList\domain\services\event\WaitListEventMeta;
 
 defined('EVENT_ESPRESSO_VERSION') || exit;
 
@@ -25,64 +23,31 @@ abstract class WaitListCommandHandler extends CommandHandler
 
 
     /**
-     * @param EE_Event $event
-     * @return int
-     * @throws EE_Error
+     * @param WaitListEventMeta $event_meta
      */
-    protected function getRegCount(EE_Event $event)
+    private $event_meta;
+
+
+
+    /**
+     * WaitListCommandHandler constructor.
+     *
+     * @param WaitListEventMeta $event_meta
+     */
+    public function __construct(WaitListEventMeta $event_meta)
     {
-        return absint(
-            $event->get_extra_meta(Constants::REG_COUNT_META_KEY, true, 0)
-        );
+        $this->event_meta = $event_meta;
     }
 
 
 
     /**
-     * @param EE_Event $event
-     * @return array
-     * @throws EE_Error
-     */
-    protected function getPromotedRegIdsArray(EE_Event $event)
-    {
-        $promoted_reg_ids = $event->get_extra_meta(
-            Constants::PROMOTED_REG_IDS_META_KEY, false, array(array())
-        );
-        return reset($promoted_reg_ids);
-    }
-
-
-
-    /**
-     * @param EE_Event $event
      * @return mixed
-     * @throws EE_Error
      */
-    protected function getPromoteWaitListRegistrants(EE_Event $event)
+    public function eventMeta()
     {
-        return filter_var(
-            $event->get_extra_meta(
-                Constants::PROMOTE_WAIT_LIST_REGISTRANTS_META_KEY,
-                true,
-                true
-            ),
-            FILTER_VALIDATE_BOOLEAN
-        );
+        return $this->event_meta;
     }
 
-
-
-    /**
-     * @param EE_Event $event
-     * @return boolean
-     * @throws EE_Error
-     */
-    protected function getAutoPromote(EE_Event $event)
-    {
-        return filter_var(
-            $event->get_extra_meta(Constants::AUTO_PROMOTE_META_KEY, true, false),
-            FILTER_VALIDATE_BOOLEAN
-        );
-    }
 
 }

1e27f37eeb70d7929899499261035f4d08ec5f24
Brent Christensen <brent@eventespresso.com>
2017-05-15 17:53:19 -0700

    use WaitListEventMeta instead of accessing event meta directly; extract logic from removeFromPromotedRegIdList() and move into new WaitListEventMeta::removeRegistrationFromPromotedRegIdsArray() method; [touch:10638]

--- domain/services/commands/UpdateRegistrationWaitListMetaDataCommandHandler.php
+++ domain/services/commands/UpdateRegistrationWaitListMetaDataCommandHandler.php
@@ -47,20 +47,20 @@ class UpdateRegistrationWaitListMetaDataCommandHandler extends WaitListCommandHa
         $new_STS_ID = $command->getNewStatusId();
         $wait_list_reg_count = null;
         if ($old_STS_ID === EEM_Registration::status_id_wait_list) {
-            $wait_list_reg_count = $this->getRegCount($event);
+            $wait_list_reg_count = $this->eventMeta()->getRegCount($event);
             $wait_list_reg_count--;
-            $event->update_extra_meta(Constants::REG_COUNT_META_KEY, $wait_list_reg_count);
+            $this->eventMeta()->updateRegCount($event, $wait_list_reg_count);
             // if new status is Approved or Pending Payment, then YAY!!!
             if (in_array($new_STS_ID, EEM_Registration::reg_statuses_that_allow_payment(), true)) {
                 $this->addMetaDataWhenRegistrationPromoted($registration, $event, $new_STS_ID);
             } else {
                 // this guy ain't going to the event EVER !!!
-                $this->addMetaDataWhenRegistrationRemoved($registration);
+                $this->addMetaDataWhenRegistrationRemoved($registration, $event);
             }
         } elseif ($new_STS_ID === EEM_Registration::status_id_wait_list) {
-            $wait_list_reg_count = $this->getRegCount($event);
+            $wait_list_reg_count = $this->eventMeta()->getRegCount($event);
             $wait_list_reg_count++;
-            $event->update_extra_meta(Constants::REG_COUNT_META_KEY, $wait_list_reg_count);
+            $this->eventMeta()->updateRegCount($event, $wait_list_reg_count);
             // if old status was Approved or Pending Payment, but they are being moved to the Wait List
             if (in_array($old_STS_ID, EEM_Registration::reg_statuses_that_allow_payment(), true)) {
                 $this->addMetaDataWhenRegistrationDemoted($registration, $event, $old_STS_ID);
@@ -70,28 +70,18 @@ class UpdateRegistrationWaitListMetaDataCommandHandler extends WaitListCommandHa
         } elseif ($old_STS_ID === EEM_Registration::status_id_pending_payment) {
             // don't need to track this reg anymore,
             // because they were either approved or cancelled altogether
-            $this->removeFromPromotedRegIdList($registration, $event);
+            $this->eventMeta()->removeRegistrationFromPromotedRegIdsArray($registration, $event);
         }
-        $perform_sold_out_status_check = $event->get_extra_meta(
-            Constants::PERFORM_SOLD_OUT_STATUS_CHECK_META_KEY,
-            true,
-            true
-        );
+        $perform_sold_out_status_check = $this->eventMeta()->getPerformSoldOutStatusCheck($event);
         if ($wait_list_reg_count !== null && $perform_sold_out_status_check) {
             // updating the reg status will trigger a sold out status check on the event,
             // which in turn will trigger WaitListMonitor::promoteWaitListRegistrants()
             // so let's turn that off while we do this, otherwise this registration
             // could just get set right back to the status it was previously at,
             // which can make it impossible to manually move a registration back to the wait list
-            $event->update_extra_meta(
-                Constants::PROMOTE_WAIT_LIST_REGISTRANTS_META_KEY,
-                false
-            );
+            $this->eventMeta()->updatePromoteWaitListRegistrants($event, false);
             $event->perform_sold_out_status_check();
-            $event->update_extra_meta(
-                Constants::PROMOTE_WAIT_LIST_REGISTRANTS_META_KEY,
-                true
-            );
+            $this->eventMeta()->updatePromoteWaitListRegistrants($event, true);
             return true;
         }
         return false;
@@ -129,7 +119,7 @@ class UpdateRegistrationWaitListMetaDataCommandHandler extends WaitListCommandHa
         // Approved registrations are guaranteed a space right away,
         // but we need a way to track registrations that were promoted from the wait list to pending payment
         if ($new_STS_ID === EEM_Registration::status_id_pending_payment) {
-            $promoted_reg_ids = $this->getPromotedRegIdsArray($event);
+            $promoted_reg_ids = $this->eventMeta()->getPromotedRegIdsArray($event);
             $promoted_reg_ids[$registration->ID()] = current_time('mysql', true);
             $event->update_extra_meta(
                 Constants::PROMOTED_REG_IDS_META_KEY,
@@ -154,22 +144,8 @@ class UpdateRegistrationWaitListMetaDataCommandHandler extends WaitListCommandHa
         );
         // don't track this registration anymore since they are back on the wait list
         if ($old_STS_ID === EEM_Registration::status_id_pending_payment) {
-            $this->removeFromPromotedRegIdList($registration, $event);
+            $this->eventMeta()->removeRegistrationFromPromotedRegIdsArray($registration, $event);
         }
-    }
-
-
-
-    /**
-     * @param EE_Registration $registration
-     * @throws EE_Error
-     */
-    private function addMetaDataWhenRegistrationRemoved(EE_Registration $registration)
-    {
-        $registration->add_extra_meta(
-            Constants::REG_REMOVED_META_KEY,
-            current_time('mysql', true)
-        );
     }
 
 
@@ -179,17 +155,14 @@ class UpdateRegistrationWaitListMetaDataCommandHandler extends WaitListCommandHa
      * @param EE_Event        $event
      * @throws EE_Error
      */
-    private function removeFromPromotedRegIdList(EE_Registration $registration, EE_Event $event)
+    private function addMetaDataWhenRegistrationRemoved(EE_Registration $registration, EE_Event $event)
     {
-        // get list of IDs for Pending Payment registrations promoted from wait list
-        $promoted_reg_ids = $this->getPromotedRegIdsArray($event);
-        //remove this registration
-        unset($promoted_reg_ids[$registration->ID()]);
-        // resave the list of Reg IDs
-        $event->update_extra_meta(
-            Constants::PROMOTED_REG_IDS_META_KEY,
-            $promoted_reg_ids
+        $this->eventMeta()->removeRegistrationFromPromotedRegIdsArray($registration, $event);
+        $registration->add_extra_meta(
+            Constants::REG_REMOVED_META_KEY,
+            current_time('mysql', true)
         );
     }
+
 
 }

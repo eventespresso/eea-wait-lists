3ca742baa5a3e62fcc22646cd640aa9a58feb396
Brent Christensen <brent@eventespresso.com>
2017-05-15 20:55:08 -0700

    WaitListRegistrationMeta: class for interacting with wait list related Registration meta; [touch:10638]

---
+++ domain/services/registration/WaitListRegistrationMeta.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace EventEspresso\WaitList\domain\services\registration;
+
+use EE_Error;
+use EE_Registration;
+use EventEspresso\WaitList\domain\Constants;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class WaitListRegistrationMeta
+ * class for interacting with wait list related Registration meta
+ *
+ * @package       Event Espresso
+ * @author        Brent Christensen
+ * @since         $VID:$
+ */
+class WaitListRegistrationMeta
+{
+
+    /**
+     * @param EE_Registration $registration
+     * @return mixed
+     * @throws EE_Error
+     */
+    public function getRegistrationSignedUp(EE_Registration $registration)
+    {
+        return $registration->get_extra_meta(Constants::REG_SIGNED_UP_META_KEY, true);
+    }
+
+
+
+    /**
+     * @param EE_Registration $registration
+     * @return mixed
+     * @throws EE_Error
+     */
+    public function addRegistrationSignedUp(EE_Registration $registration)
+    {
+        return $registration->add_extra_meta(
+            Constants::REG_SIGNED_UP_META_KEY,
+            current_time('mysql', true),
+            true
+        );
+    }
+
+
+
+    /**
+     * @param EE_Registration $registration
+     * @return bool
+     * @throws EE_Error
+     */
+    public function addRegistrationPromoted(EE_Registration $registration)
+    {
+        return $registration->add_extra_meta(
+            Constants::REG_PROMOTED_META_KEY,
+            current_time('mysql', true)
+        );
+    }
+
+
+
+    /**
+     * @param EE_Registration $registration
+     * @return bool
+     * @throws EE_Error
+     */
+    public function addRegistrationDemoted(EE_Registration $registration)
+    {
+        return $registration->add_extra_meta(
+            Constants::REG_DEMOTED_META_KEY,
+            current_time('mysql', true)
+        );
+    }
+
+
+
+    /**
+     * @param EE_Registration $registration
+     * @return bool
+     * @throws EE_Error
+     */
+    public function addRegistrationRemoved(EE_Registration $registration)
+    {
+        return $registration->add_extra_meta(
+            Constants::REG_REMOVED_META_KEY,
+            current_time('mysql', true)
+        );
+    }
+
+
+
+}

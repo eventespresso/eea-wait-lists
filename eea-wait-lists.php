94545d209b0cd1790066cb692da6c448665bb25c
EE DevBox Server <admin@eventespresso.com>
2017-08-18 00:32:40 -0400

    Bumping version to 1.0.0.dev.039. Previous Commit message: "Merge remote-tracking branch 'origin/master'
    "

--- eea-wait-lists.php
+++ eea-wait-lists.php
@@ -3,7 +3,7 @@
   Plugin Name: Event Espresso - Wait Lists (EE4.9+)
   Plugin URI: http://www.eventespresso.com
   Description: The Event Espresso Wait Lists Addon maximizes event sales by allowing attendees to partially register for a datetime or ticket that has sold out, but then complete the registration process later after spaces have become available due to venue change, additional tickets, non-payment, cancellation, etc.
-  Version: 1.0.0.dev.038
+  Version: 1.0.0.dev.039
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
   Copyright 2014 Event Espresso (email : support@eventespresso.com)

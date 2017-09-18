b9af51a46e0d87135cfb834a4e18211e1e39d27c
Darren Ethier <darren@roughsmootheng.in>
2017-09-18 04:23:22 -0400

    add mising context label [touch: 10991]

--- domain/entities/EE_Registration_Added_To_Waitlist_message_type.class.php
+++ domain/entities/EE_Registration_Added_To_Waitlist_message_type.class.php
@@ -33,6 +33,11 @@ class EE_Registration_Added_To_Waitlist_message_type extends EE_Registration_Bas
      */
     public function _set_contexts()
     {
+        $this->_context_label = array(
+            'label'       => esc_html__('recipient', 'event_espresso'),
+            'plural'      => esc_html__('recipients', 'event_espresso'),
+            'description' => esc_html__("Recipient's are who will receive the message.", 'event_espresso'),
+        );
         $this->_contexts = array(
             'attendee' => array(
                 'label'       => esc_html__('Registrant', 'event_espresso'),

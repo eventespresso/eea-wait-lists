138501e52a0592be2d385e6040fb6ed5829813a1
Brent Christensen <brent@eventespresso.com>
2017-09-21 18:17:35 -0700

    don't use IDs to target elements; [touch:11032]
    formatting;
    add debug code;

--- assets/wait_list.js
+++ assets/wait_list.js
@@ -1,18 +1,22 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
 
-    $('.ee-join-wait-list-btn').on('click', function (e) {
+    $('.ee-join-wait-list-btn').on('click', function(e) {
+        // console_log('wait list button', $(this).attr('id'), true);
         e.preventDefault();
         e.stopPropagation();
-        var left = 0;
-        var BackgroundColor = '#fff';
-        var $wait_list_parent = $('#event_wait_list-frm').parent();
-        if ($wait_list_parent.length){
+        var left              = 0;
+        var BackgroundColor   = '#fff';
+        var $wait_list_parent = $(this).closest('form');
+        // console_log('wait list form', $wait_list_parent.attr('id'), false);
+        if ($wait_list_parent.length) {
             BackgroundColor = eeGetParentBackgroundColor($wait_list_parent);
             BackgroundColor = eeRgbToHex(BackgroundColor);
             $wait_list_parent.css({'background': BackgroundColor});
             left = $wait_list_parent.innerWidth();
         }
         var $wait_list_form = $('#' + $(this).data('inputs'));
+        // console_log('wait list form inputs', $wait_list_form.attr('id'), false);
+
         if (parseFloat(left)) {
             left = (left - $wait_list_form.outerWidth()) / 2;
         } else {
@@ -21,13 +25,12 @@ jQuery(document).ready(function ($) {
         $wait_list_form.css({'background': BackgroundColor, 'left': left + 'px'}).show();
     });
 
-    $('.ee-wait-list-cancel-lnk').on('click', function (e) {
+    $('.ee-wait-list-cancel-lnk').on('click', function(e) {
         e.preventDefault();
         e.stopPropagation();
         var wait_list_form_id = '#' + $(this).data('inputs');
+        // console_log('wait_list_form_id', wait_list_form_id, true);
         $(wait_list_form_id).hide();
     });
-
-
 
 });

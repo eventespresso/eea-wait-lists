226e0ffd2c02b10690ced48966e195b93e559f47
Brent Christensen <brent@eventespresso.com>
2017-09-21 17:34:04 -0700

    WaitListFormHandlerCollection; [touch:11032]
    a Collection of WaitListFormHandler objects for events with active Wait Lists;

---
+++ domain/services/collections/WaitListFormHandlerCollection.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EventEspresso\WaitList\domain\services\collections;
+
+use EventEspresso\core\exceptions\InvalidInterfaceException;
+use EventEspresso\core\services\collections\Collection;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class WaitListFormHandlerCollection
+ * a Collection of WaitListFormHandler objects for events with active Wait Lists
+ *
+ * @package EventEspresso\WaitList\domain\services\collections
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+class WaitListFormHandlerCollection extends Collection
+{
+
+    /**
+     * WaitListFormHandlerCollection constructor.
+     *
+     * @throws InvalidInterfaceException
+     */
+    public function __construct()
+    {
+        parent::__construct('EventEspresso\WaitList\domain\services\forms\WaitListFormHandler');
+    }
+}
+// Location: WaitListFormHandlerCollection.php

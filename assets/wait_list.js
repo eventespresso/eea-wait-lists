8c0e75486c4c9c64b9405960c0c665607e0971f9
Brent Christensen <brent@eventespresso.com>
2017-10-04 12:20:28 -0700

    extract logic for displaying form into function; trigger form display if opened from iframe;  [touch:11039]

--- assets/wait_list.js
+++ assets/wait_list.js
@@ -1,12 +1,9 @@
 jQuery(document).ready(function($) {
 
-    $('.ee-join-wait-list-btn').on('click', function(e) {
-        // console_log('wait list button', $(this).attr('id'), true);
-        e.preventDefault();
-        e.stopPropagation();
+    var display_event_list_form = function($trigger) {
         var left              = 0;
         var BackgroundColor   = '#fff';
-        var $wait_list_parent = $(this).closest('form');
+        var $wait_list_parent = $trigger.closest('form');
         // console_log('wait list form', $wait_list_parent.attr('id'), false);
         if ($wait_list_parent.length) {
             BackgroundColor = eeGetParentBackgroundColor($wait_list_parent);
@@ -14,8 +11,8 @@ jQuery(document).ready(function($) {
             $wait_list_parent.css({'background': BackgroundColor});
             left = $wait_list_parent.innerWidth();
         }
-        var $wait_list_form = $('#' + $(this).data('inputs'));
         // console_log('wait list form inputs', $wait_list_form.attr('id'), false);
+        var $wait_list_form = $('#' + $trigger.data('inputs'));
 
         if (parseFloat(left)) {
             left = (left - $wait_list_form.outerWidth()) / 2;
@@ -23,14 +20,30 @@ jQuery(document).ready(function($) {
             left = 0;
         }
         $wait_list_form.css({'background': BackgroundColor, 'left': left + 'px'}).show();
+    };
+
+    //  display form when Join button is clicked
+    $('.ee-join-wait-list-btn').on('click', function(e) {
+        // console_log('wait list button', $(this).attr('id'), true);
+        e.preventDefault();
+        e.stopPropagation();
+        display_event_list_form($(this));
     });
 
+    //  close form when cancel link is clicked
     $('.ee-wait-list-cancel-lnk').on('click', function(e) {
         e.preventDefault();
         e.stopPropagation();
         var wait_list_form_id = '#' + $(this).data('inputs');
         // console_log('wait_list_form_id', wait_list_form_id, true);
         $(wait_list_form_id).hide();
+    });
+
+    //  display form if opened from iframe
+    $('.ee-display-wait-list-form').each(function() {
+        if ($(this).val() === '1') {
+            display_event_list_form($(this));
+        }
     });
 
 });

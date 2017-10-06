be0308d457098dcc08e29eb40d85cd526d41c6d6
Brent Christensen <brent@eventespresso.com>
2017-10-06 11:07:22 -0700

    WaitListCheckoutMonitorMock   [touch:11061]

---
+++ tests/mocks/WaitListCheckoutMonitorMock.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace EventEspresso\WaitList\tests\mocks;
+
+use EventEspresso\WaitList\domain\services\checkout\WaitListCheckoutMonitor;
+use EventEspresso\WaitList\domain\services\registration\WaitListRegistrationMeta;
+
+defined('EVENT_ESPRESSO_VERSION') || exit;
+
+
+
+/**
+ * Class WaitListCheckoutMonitor
+ * Description
+ *
+ * @author  Brent Christensen
+ * @since   $VID:$
+ */
+class WaitListCheckoutMonitorMock extends WaitListCheckoutMonitor
+{
+
+    /**
+     * @return WaitListRegistrationMeta
+     */
+    public function getRegistrationMeta()
+    {
+        return $this->registration_meta;
+    }
+
+
+}
+// Location: WaitListCheckoutMonitor.php

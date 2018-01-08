3c84ec75cd40de976b3bf36f263be70a4ec06d66
Darren Ethier <darren@roughsmootheng.in>
2018-01-08 16:10:27 -0500

    acceptance test fixes [touch: 11111]
    [ci skip]

--- acceptance_tests/tests/f-TestManualPromotionFromWaitListNotificationCept.php
+++ acceptance_tests/tests/f-TestManualPromotionFromWaitListNotificationCept.php
@@ -4,19 +4,20 @@ use Page\MessagesAdmin;
 use Page\WaitListsGeneral;
 
 /**
- * This test covers manually setting a registration to waitlist status and that it receives the Registration Demoted
- * To Wait List Notification
+ * This test covers manually changing a registration from RWL to RPP and that it receives the Registration Promoted
+ * From Wait List Notification
  */
 $I = new EventEspressoAddonAcceptanceTester($scenario, WaitListsGeneral::ADDDON_SLUG_FOR_WP_PLUGIN_PAGE, false);
-$I->wantTo('Test that manually setting the status of a registration to wait list (RWL) sends the appropriate notifications.');
+$I->wantTo('Test that manually setting the status of a registration to RPP from RWL sends the appropriate notifications.');
 
 $I->amGoingTo(
-    'Go to registration admin page and change the status of a registration from Group C (created in previous testcases)to RWL.'
+    'Go to registration admin page and change the status of a registration from Group  (created in previous testcases)to RWL.'
 );
 $I->loginAsAdmin();
 $I->amOnDefaultRegistrationsListTableAdminPage();
+$I->searchForRegistrationOnRegistrationListTableWithText('Group C');
 $I->selectBulkActionCheckboxesForRegistrationIds(array(15));
-$I->submitBulkActionOnListTable('Move Registrations To Wait List and Notify');
+$I->submitBulkActionOnListTable('wait_list_and_notify_registrations');
 $I->waitForText('Registration status has been set to wait list');
 $I->see('Messages have been successfully queued for generation and sending.');
 
